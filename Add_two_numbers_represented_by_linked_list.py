6df5fdecc1af2642aff6e0acd7b8f63c8b075f49
asldev <eshonkulovasliddindeveloper@gmail.com>
2022-05-24 08:03:10 +0500

    changes

---
+++ Add_two_numbers_represented_by_linked_list.py
@@ -0,0 +1,46 @@
+''' Node for linked list:
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+'''
+
+
+class Solution:
+    # Function to add two numbers represented by linked list.
+    def addTwoLists(self, first, second):
+        # code here
+        # return head of sum list
+        str1 = ''
+        str2 = ''
+        lis1 = []
+        lis2 = []
+        lis3 = []
+        first_ll = first
+        second_ll = second
+
+        while first_ll:
+            lis1.append(first_ll.data)
+            first_ll = first_ll.next
+
+        while second_ll:
+            lis2.append(second_ll.data)
+            second_ll = second_ll.next
+
+        for i in lis1:
+            str1 += str(i)
+
+        for i in lis2:
+            str2 += str(i)
+
+        sum_num = int(str1) + int(str2)
+
+        for i in str(sum_num):
+            lis3.append(i)
+
+        new_ll = LinkedList()
+        for i in lis3:
+            new_ll.insert(i)
+        return new_ll.head
