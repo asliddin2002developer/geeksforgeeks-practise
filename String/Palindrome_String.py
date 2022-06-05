9094ecbea09646b7e1b0c17354db3a28156077bc
asldev <eshonkulovasliddindeveloper@gmail.com>
2022-06-05 09:51:17 +0500

    new excersice.

--- String/Palindrome_String.py
+++ String/Palindrome_String.py
@@ -1,17 +1,23 @@
 class Solution:
     def isPalindrome(self, S):
-        i = 0
-        j = len(S)-1
-        S = S.lower()
-        while i < j:
-            while not S[i].isalnum() and i < j:
-                i += 1
-            while not S[j].isalnum() and i < j:
-                j -= 1
-            if S[i] != S[j]:
-                return 0
-            i+=1
-            j -=1
-        return 1
+        # 		i = 0
+        # 		j = len(S)-1
+        # 		S = S.lower()
+        # 		while i < j:
+        # 		    while not S[i].isalnum() and i < j:
+        # 		        i += 1
+        # 		    while not S[j].isalnum() and i < j:
+        # 		        j -= 1
+        # 		    if S[i] != S[j]:
+        # 		        return 0
+        # 		    i+=1
+        # 		    j -=1
+        # 		return 1
+
+        ##second solution
+        if S == S[::-1]:
+            return 1
+        else:
+            return 0
 
 
