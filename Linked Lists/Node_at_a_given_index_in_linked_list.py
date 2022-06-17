7d06ff53d4eb7cecd29413fb2a7507ef0eca8c9f
asliddin2002developer <eshonkulovasliddindevloper@gmail.com>
2022-06-17 21:08:50 +0500

    new exercise...

---
+++ Linked Lists/Node_at_a_given_index_in_linked_list.py
@@ -0,0 +1,23 @@
+"""index is the node which is to be displayed in output
+  Node is defined as
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+# Linked List class contains a Node object
+class LinkedList:
+    def __init__(self):
+        self.head = None
+This is method only submission.
+ You only need to complete below method.
+"""
+
+''' My solution '''
+def getNth(head, k):
+    # Code here
+    cur = head
+    count = 1
+    while cur and count!=k:
+        cur = cur.next
+        count += 1
+    return cur.data
